0a74b27d9c11815ad7e5933e73c0eb077575dca2
Pierre Irrmann <pierre.irrmann@gmail.com>
2022-05-26 06:52:59 +0200

    Initial benchmark to show poor Arrray2D equality perff

---
+++ tests/benchmarks/PrimitivesBenchmarks/GenericEquality.fs
@@ -0,0 +1,32 @@
+﻿namespace PrimitivesBenchmarks
+
+open System
+open System.Collections
+
+open BenchmarkDotNet.Attributes
+
+module Array2DEquality =
+
+    type [<Measure>] m
+
+    [<MemoryDiagnoser>]
+    type Benchmark() =
+
+        let values1 =
+            let rand = new Random(42)
+            Array.init 10 (fun _i -> Array2D.init 40 40 (fun _x _y -> rand.NextDouble() |> LanguagePrimitives.FloatWithMeasure<m^2>))
+        let values2 = values1 |> Array.map (Array2D.copy)
+
+        [<Benchmark>]
+        member this.SelfEqualityOfArray2DFloat() =
+            let mutable result = true
+            for i = 0 to 9 do
+                result <- values1.[i] = values1.[i]
+            if not result then failwith "Incorrect"
+
+        [<Benchmark>]
+        member this.CopyEqualityOfArray2DFloat() =
+            let mutable result = true
+            for i = 0 to 9 do
+                result <- values1.[i] = values2.[i]
+            if not result then failwith "Incorrect"
---
+++ tests/benchmarks/PrimitivesBenchmarks/Program.fs
@@ -0,0 +1,11 @@
+﻿namespace PrimitivesBenchmarks
+
+open BenchmarkDotNet.Running
+
+module Main =
+
+    [<EntryPoint>]
+    let main argv =
+        printfn "Running benchmarks..."
+        let results = BenchmarkRunner.Run<Array2DEquality.Benchmark>()
+        0
